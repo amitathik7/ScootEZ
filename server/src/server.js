6a41577510669401116dc41a915868d50f2173eb
Jaidyn Holt <jaidynholt2587@gmail.com>
2024-06-27 23:04:02 -0400

    Set up the server backend and react app frontend

---
+++ server/src/server.js
@@ -0,0 +1,20 @@
+//===============Setup backend API===============
+const express = require('express')  // import express
+//import { config } from 'dotenv';
+//import { executeCrudOperations } from './databaseCRUD';
+const app = express()   // create app
+
+// get variables from .env into process.env
+//config();
+//console.log(process.env.DB_URI);
+
+// setup route for api
+app.get("/api", (request, response) => {
+    response.json({"users": ["user1, user2, user3"]})   // "users" is a JSON array of users
+})
+
+// startup backend
+app.listen(5000, () => {console.log("Server started on port 5000")})  // server runs on port 5000, client (React) runs on port 3000
+
+// call functions for CRUD from database
+//await executeCrudOperations();
