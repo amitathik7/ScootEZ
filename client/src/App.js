70db64ccc5f7f72962e4e6671df5a90427572371
ybnelan <elanbarnur@gmail.com>
2024-07-09 08:27:13 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import styles from "./styles.css";
 import HomePage from "./Home.js";
 import LoginPage from "./Login.js";
 
+// test change
+
 function App() {
   // The display case for the screen
   // 0=home
