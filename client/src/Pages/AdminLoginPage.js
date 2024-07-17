d4c9d157f569e227e2e8ea9fb60e55193ab952d7
annabel-stocksnatalias <astocksnatalias@ufl.edu>
2024-07-16 20:48:19 -0400

    login features added

--- client/src/Pages/AdminLoginPage.js
+++ client/src/Pages/AdminLoginPage.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useContext, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import styles from "../styles.css"; // Make sure to import your styles
+import { IsLoggedInContext } from '../App.js';
+import styles from "../styles.css";
 
 export default function AdminLoginPage() {
+    const { isLoggedIn, setIsLoggedIn } = useContext(IsLoggedInContext);
+
+    // states
+    const [isInvalidCredentials, setIsInvalidCredentials] = useState(false);
+    const [isLoginButtonActive, setIsLoginButtonActive] = useState(false);
+
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+
+    // Profile state variables for logging in
     const [loginInfo, setLoginInfo] = useState({
         email: '',
         password: '',
@@ -40,38 +51,86 @@ export default function AdminLoginPage() {
             );
 
             if (response.ok) {
-                // Successful login
+                const data = await response.json(); 
+                localStorage.setItem("token", data.token);
+                setIsLoggedIn(true);
                 navigate('/admin-dashboard'); // Redirect to admin dashboard
             } else {
-                // Handle failed login (e.g., show error message)
+                setIsInvalidCredentials(true); 
                 console.error('Admin Login failed');
             }
         } catch (error) {
+            setIsInvalidCredentials(true);
             console.error('Error during admin login:', error);
         }
     };
+
+    function ValidateAllFields() {
+        if (loginInfo.email.length <= 0 || loginInfo.password.length <= 0) {
+            setIsLoginButtonActive(false);
+        } else if (emailRef.current.validity.valid &&
+            passwordRef.current.validity.valid) {
+            setIsLoginButtonActive(true);
+        } else {
+            setIsLoginButtonActive(false);
+        }
+    }
+
+    function ToggleShowPassword() {
+        console.log("toggle clicked");
+        if (passwordRef.current.type === "password") {
+            passwordRef.current.type = "text";
+        } else {
+            passwordRef.current.type = "password";
+        }
+    }
+
+    // shows the invalid message only if the showPasswordMessage state is true
+    function InvalidCredentialsMessage() {
+        if (isInvalidCredentials) {
+            return (
+                <p className="warningText">
+                    &#9888; The email or password entered is invalid.
+                </p>
+            );
+        } else {
+            return (
+                <div />
+            );
+        }
+    }
 
     return (
         <div style={{ width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center", margin: "0px", paddingTop: "150px", paddingBottom: "150px" }}>
             <div style={{ width: "60%", display: "inline-block", lineHeight: "40px" }}>
                 <h1>Administrator Login</h1>
                 <p>Email:</p>
-                <input className="input1"
+                <input className="input1" required
+                    type="email"
+                    placeholder="Email"
+                    ref={emailRef}
                     value={loginInfo.email}
                     onChange={handleEmailChange}
-                 />
+                    onInput={ValidateAllFields}
+                />
                 <p>Password:</p>
-                <input className="input1"
+                <input className="input1" required
                     type="password"
+                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                    placeholder="Password"
+                    ref={passwordRef}
                     value={loginInfo.password}
                     onChange={handlePasswordChange}
-                />
+                    onInput={ValidateAllFields}
+                /> <br />
+                <input type="checkbox" onClick={ToggleShowPassword} /> Show Password
+                <br />
                 <div style={{ marginTop: '20px' }}>
-          <button className="button1" onClick={handleLogin}>
-            LOGIN
-          </button>
+                    <button className="button1" onClick={handleLogin} disabled={!isLoginButtonActive}>
+                        LOGIN
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
--- client/src/Pages/EmployeeLoginPage.js
+++ client/src/Pages/EmployeeLoginPage.js
@@ -1,11 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { IsLoggedInContext } from '../App.js';
+import styles from "../styles.css";
 
 export default function EmployeeLoginPage() {
+    // global context
+    const { isLoggedIn, setIsLoggedIn } = useContext(IsLoggedInContext);
+
+    // states
+    const [isInvalidCredentials, setIsInvalidCredentials] = useState(false);
+    const [isLoginButtonActive, setIsLoginButtonActive] = useState(false);
+
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+
+    // Profile state variables for logging in
     const [loginInfo, setLoginInfo] = useState({
         email: '',
         password: '',
     });
+
 
     const handleEmailChange = (e) => {
         setLoginInfo({
@@ -20,6 +34,7 @@ export default function EmployeeLoginPage() {
             password: e.target.value
         });
     };
+
 
     const navigate = useNavigate();
 
@@ -39,38 +54,87 @@ export default function EmployeeLoginPage() {
             );
 
             if (response.ok) {
-                // Successful login
+                const data = await response.json();
+                localStorage.setItem("token", data.token);
+                setIsLoggedIn(true);
                 navigate('/employee-dashboard'); 
             } else {
-                // Handle failed login 
+                setIsInvalidCredentials(true); 
                 console.error('Login failed');
             }
         } catch (error) {
+            setIsInvalidCredentials(true);
             console.error('Error during login:', error);
         }
     };
+
+    function ValidateAllFields() {
+        if (loginInfo.email.length <= 0 || loginInfo.password.length <= 0) {
+            setIsLoginButtonActive(false);
+        } else if (emailRef.current.validity.valid &&
+            passwordRef.current.validity.valid) {
+            setIsLoginButtonActive(true);
+        } else {
+            setIsLoginButtonActive(false);
+        }
+    }
+
+    function ToggleShowPassword() {
+        console.log("toggle clicked");
+        if (passwordRef.current.type === "password") {
+            passwordRef.current.type = "text";
+        } else {
+            passwordRef.current.type = "password";
+        }
+    }
+
+    // shows the invalid message only if the showPasswordMessage state is true
+    function InvalidCredentialsMessage() {
+        if (isInvalidCredentials) {
+            return (
+                <p className="warningText">
+                    &#9888; The email or password entered is invalid.
+                </p>
+            );
+        } else {
+            return (
+                <div />
+            );
+        }
+    }
 
     return (
         <div style={{ width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center", margin: "0px", paddingTop: "150px", paddingBottom: "150px" }}>
             <div style={{ width: "60%", display: "inline-block", lineHeight: "40px" }}>
                 <h1>Employee Login</h1>
+                <InvalidCredentialsMessage />
                 <p>Email:</p>
-                <input className="input1"
+                <input className="input1" required
+                    type="email"
+                    placeholder="Email"
+                    ref={emailRef}
                     value={loginInfo.email}
                     onChange={handleEmailChange}
-                 />
+                    onInput={ValidateAllFields}
+                />
                 <p>Password:</p>
-                <input className="input1"
+                <input className="input1" required
                     type="password"
+                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                    placeholder="Password"
+                    ref={passwordRef}
                     value={loginInfo.password}
                     onChange={handlePasswordChange}
-                />
-               <div style={{ marginTop: '20px' }}> 
-          <button className="button1" onClick={handleLogin}>
-            LOGIN
-          </button>
+                    onInput={ValidateAllFields}
+                /> <br />
+                <input type="checkbox" onClick={ToggleShowPassword} /> Show Password
+                <br />
+                <div style={{ marginTop: '20px' }}>
+                    <button className="button1" onClick={handleLogin} disabled={!isLoginButtonActive}>
+                        LOGIN
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
