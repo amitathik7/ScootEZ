51dd4a977616073c0441d434ca5b6d702886933b
Jaidyn Holt <jaidynholt2587@gmail.com>
2024-07-25 22:11:02 -0400

    Updated the password and delete on the profile page, fixed the data population, fixed the page authentication

--- client/src/Pages/ProfilePage.js
+++ client/src/Pages/ProfilePage.js
@@ -10,17 +10,18 @@ export default function ProfilePage() {
     // global context
     const { isLoggedIn, setIsLoggedIn } = useContext(IsLoggedInContext);
 
+    const [isAuthenticated, setIsAuthenticated] = useState('fetching');
+
     async function AuthenticateUser() {
         try {    
             console.log("in the fetch request");
-          const response = await fetch('http://localhost:5000/api/token/verify', {
+            const response = await fetch('http://localhost:5000/api/token/verify', {
             method: 'GET',
             headers: {
               'Authorization': `Bearer ${localStorage.getItem("token")}`
             },
           });
           const {message} = await response.json(); // this will return true or false in "status"
-          console.log(message);
           return message;
         }
         catch (error) {
@@ -28,15 +29,8 @@ export default function ProfilePage() {
           return false;
         }
     }
-    
-    // if (localStorage.getItem("token") != null) {
-    //     console.log("authenticating...");
-    //     AuthenticateUser().then((result) => {
-    //         console.log("setting state variable")
-    //         if (result) { setIsLoggedIn(true) }
-    //         else { setIsLoggedIn(false) }
-    //     });
-    // }
+
+    const [needsAccountData, setNeedsAccountData] = useState(true);
 
     // states
     const [isFirstNameActive, setIsFirstNameActive] = useState(false);
@@ -51,50 +45,57 @@ export default function ProfilePage() {
 
 
     // Profile state variables for name (only use for creating new account)
-    const [accountInfo, setAccountInfo] = useState({
-        firstName: 'Jay',
-        lastName: 'Holt',
-        email: 'jh@mail.com',
-        password: 'encryped text here',
+    const [fieldInfo, setFieldInfo] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
         address: '',
         creditCard: ''
     });
     function handleFirstNameChange(e) {
-        setAccountInfo({
-            ...accountInfo,
+        setFieldInfo({
+            ...fieldInfo,
             firstName: e.target.value
         });
     }
     function handleLastNameChange(e) {
-        setAccountInfo({
-            ...accountInfo,
+        setFieldInfo({
+            ...fieldInfo,
             lastName: e.target.value
         });
     }
     function handleEmailChange(e) {
-        setAccountInfo({
-            ...accountInfo,
+        setFieldInfo({
+            ...fieldInfo,
             email: e.target.value
         });
     }
     function handlePasswordChange(e) {
-        setAccountInfo({
-            ...accountInfo,
+        setFieldInfo({
+            ...fieldInfo,
             password: e.target.value
         });
     }
     function handleAddressChange(e) {
-        setAccountInfo({
-            ...accountInfo,
+        setFieldInfo({
+            ...fieldInfo,
             address: e.target.value
         });
     }
     function handleCardChange(e) {
-        setAccountInfo({
-            ...accountInfo,
+        setFieldInfo({
+            ...fieldInfo,
             creditCard: e.target.value
         });
     }
+
+    const [oldPassword, setOldPassword] = useState('');
+    function handleOldPasswordChange(e) {
+        setOldPassword(e.target.value);
+    }
+
+    const [oldPasswordValid, setOldPasswordValid] = useState(false);
 
     // get account information from backend
     async function getAccountData() {
@@ -114,19 +115,47 @@ export default function ProfilePage() {
         }
     }
 
-    async function SaveAccountData() {
+    // get account information from backend
+    async function validateOldPassword() {
         try {
-            console.log(accountInfo);
-
             const response = await fetch(
-                'http://localhost:5000/api/users/update',
+                "http://localhost:5000/api/users/validatePassword",
                 {
                     method: "POST",
                     headers: {
                         'Authorization': `Bearer ${localStorage.getItem("token")}`,
                         "Content-Type": "application/json",
                     },
-                    body: JSON.stringify(accountInfo)
+                    body: JSON.stringify(oldPassword)
+                }
+            );
+            if (response.ok) {
+                return await response.json(); // should return true or false
+            }
+            else {
+                console.log("invalid credentials");
+                return false;
+            }
+        }
+        catch (error) {
+            console.error("error detected: ", error);
+            return false;
+        }
+    }
+
+    async function SaveAccountData() {
+        try {
+            console.log(fieldInfo);
+
+            const response = await fetch(
+                'http://localhost:5000/api/users/update',
+                {
+                    method: "PUT",
+                    headers: {
+                        'Authorization': `Bearer ${localStorage.getItem("token")}`,
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(fieldInfo)
                 }
             );
             if (response.ok) {
@@ -167,6 +196,38 @@ export default function ProfilePage() {
         }
     }
 
+    // button that activates the password field
+    function PasswordButton() {
+        function handleClick() {
+            // clear previous entry in oldpassword
+            setOldPassword('');
+            // toggle on
+            setIsPasswordActive((props) => !props);
+        }
+        return (
+            <button className="button1"
+            style={{display: `${isPasswordActive ? "none" : "inline"}`}}
+            onClick={handleClick}>
+                CHANGE PASSWORD
+            </button>
+        );
+    }
+
+    // handle pressing "enter" on the old password input
+    function handleKeyPress (event) {
+        if(event.key === 'Enter'){
+          validateOldPassword().then((isSuccess) => {
+            if (isSuccess){
+                setOldPasswordValid(true);
+            }
+            else {
+                alert("Password is not valid");
+                setOldPasswordValid(false);
+            }
+          })
+        }
+    }
+
     // button that saves the changed account info
     function SaveChangesButton() {
         function handleClick() {
@@ -188,6 +249,16 @@ export default function ProfilePage() {
 
     // button that saves the changed account info
     function DeleteAccountButton() {
+        return (
+            <button className="button3" onClick={() => setIsConfirmationBoxOpen(true)}>
+                DELETE ACCOUNT
+            </button>
+        );
+    }
+
+    const [isConfirmationBoxOpen, setIsConfirmationBoxOpen] = useState(false);
+
+    function DeleteConformationBox() {
         const navigate = useNavigate();
         function handleClick() {
             DeleteAccount().then((isSuccess) => {
@@ -201,11 +272,28 @@ export default function ProfilePage() {
                 }
             })   
         }
-        return (
-            <button className="button3" onClick={handleClick}>
-                DELETE ACCOUNT
-            </button>
-        );
+        if (isConfirmationBoxOpen) {
+            return (
+                <div className="deleteConfirmationBox">
+                    <p>
+                        &#9888; Are you sure you want to delete your account? <br/>
+                        This is non-reversible.
+                    </p>
+                    <button className="button4" onClick={handleClick}>
+                        DELETE
+                    </button> <br/>
+                    <button className="button4" onClick={() => setIsConfirmationBoxOpen(false)}>
+                        CANCEL
+                    </button>
+                </div>
+            )
+        }
+        else {
+            return (
+                <div/>
+            );
+        }
+        
     }
 
     // shows the invalid message only if the showPasswordMessage state is true
@@ -236,7 +324,7 @@ export default function ProfilePage() {
     function ValidatePassword() {
         // validate letters
         var lowerCaseLetters = /[a-z]/g;
-        if(accountInfo.password.match(lowerCaseLetters)) {
+        if(fieldInfo.password.match(lowerCaseLetters)) {
             lowercaseRef.current.className = "valid";
         } else {
             lowercaseRef.current.className = "invalid";
@@ -244,7 +332,7 @@ export default function ProfilePage() {
 
         // Validate capital letters
         var upperCaseLetters = /[A-Z]/g;
-        if(accountInfo.password.match(upperCaseLetters)) {
+        if(fieldInfo.password.match(upperCaseLetters)) {
             capitalRef.current.className = "valid";
         } else {
             capitalRef.current.className = "invalid";
@@ -252,14 +340,14 @@ export default function ProfilePage() {
 
         // validate numbers
         var numbers = /[0-9]/g;
-        if(accountInfo.password.match(numbers)) {
+        if(fieldInfo.password.match(numbers)) {
             numberRef.current.className = "valid";
         } else {
             numberRef.current.className = "invalid";
         }
 
         // Validate length
-        if(accountInfo.password.length >= 8) {
+        if(fieldInfo.password.length >= 8) {
             lengthRef.current.className = "valid";
         } else {
             lengthRef.current.className = "invalid";
@@ -294,19 +382,80 @@ export default function ProfilePage() {
         }
     }
 
-    // if the password is hidden or shows as text
-    function ToggleShowPassword() {
-        console.log("toggle clicked");
-        if (passwordRef.current.type === "password") {
-            passwordRef.current.type = "text";
-        } else {
-            passwordRef.current.type = "password";
+    function OldPasswordInput() {
+        if (isPasswordActive) {
+            return (
+                <div>
+                    Please enter your current password:
+                </div>
+            );
+        }
+        else {
+            return (
+                <div />
+            );
         }
     }
+
+    function ShowPasswordBox() {
+        // if the password is hidden or shows as text
+        function ToggleShowPassword() {
+            console.log("toggle clicked");
+            if (passwordRef.current.type === "password") {
+                passwordRef.current.type = "text";
+            } else {
+                passwordRef.current.type = "password";
+            }
+        }
+        if (oldPasswordValid) {
+            return (
+                <div>
+                    <input type="checkbox"
+                        onClick={ToggleShowPassword}
+                    />
+                    Show Password
+                </div>
+            );
+        }
+        else {
+            return (
+                <div />
+            );
+        }
+    }
+
+
     
-    if (isLoggedIn) {
-        // call get account info immediately and set it in the state
-        getAccountData().then((data) => { setAccountInfo(data) });
+
+    if (isAuthenticated === 'fetching') {
+        // call authentification function
+        (async function(){
+            const result = await AuthenticateUser();
+            if (result) {setIsAuthenticated('true');}
+            else {setIsAuthenticated('false');}
+        })();
+
+        return (
+            <div>
+                One moment...
+            </div>
+        );
+    }
+    else if (isAuthenticated === 'true') {
+
+        if (needsAccountData) {
+            console.log("GETTING ACCOUNT DATA");
+            // call authentification function
+            (async function(){
+                const data = await getAccountData();
+                console.log("the fetched data: ");
+                console.log(data);
+
+                setFieldInfo(data);
+
+                setNeedsAccountData(false);
+            })();
+        }
 
         return (
             <div className="fullBox gray">
@@ -316,7 +465,7 @@ export default function ProfilePage() {
                     <input className="input2" disabled={ isFirstNameActive ? false : true } required
                         type="text"
                         placeholder="First Name"
-                        value={accountInfo.firstName}
+                        value={fieldInfo.firstName}
                         onChange={handleFirstNameChange}
                     />
                     <button className="button4" onClick={() => setIsFirstNameActive((props) => !props)}>
@@ -327,7 +476,7 @@ export default function ProfilePage() {
                     <input className="input2" disabled={ isLastNameActive ? false : true } required
                         type="text"
                         placeholder="Last Name"
-                        value={accountInfo.lastName}
+                        value={fieldInfo.lastName}
                         onChange={handleLastNameChange}
                     />
                     <button className="button4" onClick={() => setIsLastNameActive((props) => !props)}>
@@ -338,7 +487,7 @@ export default function ProfilePage() {
                     <input className="input2" disabled={ isEmailActive ? false : true } required
                         type="email"
                         placeholder="Email"
-                        value={accountInfo.email}
+                        value={fieldInfo.email}
                         onChange={handleEmailChange}
                     />
                     <button className="button4" onClick={() => setIsEmailActive((props) => !props)}>
@@ -346,12 +495,27 @@ export default function ProfilePage() {
                     </button>
 
                     <p>Password:</p>
-                    <input className="input2" disabled={ isPasswordActive ? false : true } required
+                    <PasswordButton />
+                    <p style={{display: `${isPasswordActive ? "inline" : "none"}`}}> <br/>Please enter your current password:</p>
+                    <input className="input2" style={{display: `${isPasswordActive ? "inline" : "none"}`}} required
+                        type="password"
+                        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                        placeholder="Current Password"
+                        value={oldPassword}
+                        onChange={handleOldPasswordChange}
+                        onKeyDown={handleKeyPress}
+                    />
+                    <button className="button4" style={{display: `${isPasswordActive ? "inline" : "none"}`}}
+                    onClick={() => setIsPasswordActive(false)}>
+                        CANCEL
+                    </button>
+
+                    <input className="input2" style={{display: `${oldPasswordValid ? "inline" : "none"}`}} required
                         type="password"
                         pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
                         placeholder="Password"
                         ref={passwordRef}
-                        value={accountInfo.password}
+                        value={fieldInfo.password}
                         onChange={handlePasswordChange}
                         onFocus={() => {setShowPasswordMessage(true)}}
                         onBlur={() => {
@@ -359,18 +523,19 @@ export default function ProfilePage() {
                         }}
                         onKeyUp={ValidatePassword}
                     />
-                    <button className="button4" onClick={() => setIsPasswordActive((props) => !props)}>
+                    <button className="button4" style={{display: `${oldPasswordValid ? "inline" : "none"}`}}
+                        onClick={() => setIsPasswordActive((props) => !props)}>
                         <PencilIcon style={{ height: "30px", fill: "black" }} />
                     </button>
                     <br/>
-                    <input type="checkbox" onClick={ToggleShowPassword}/> Show Password
-                    <PasswordMessage />
+                    <ShowPasswordBox/>
+                    <PasswordMessage/>
 
                     <p>Address:</p>
                     <input className="input2" disabled={ isAddressActive ? false : true }
                         type="text"
                         placeholder="Address"
-                        value={accountInfo.address}
+                        value={fieldInfo.address}
                         onChange={handleAddressChange}
                     />
                     <button className="button4" onClick={() => setIsAddressActive((props) => !props)}>
@@ -381,7 +546,7 @@ export default function ProfilePage() {
                     <input className="input2" disabled={ isCardActive ? false : true}
                         type="text"
                         placeholder="FIXME - multiple fields"
-                        value={accountInfo.creditCard}
+                        value={fieldInfo.creditCard}
                         onChange={handleCardChange}
                     />
                     <button className="button4" onClick={() => setIsCardActive((props) => !props)}>
@@ -389,6 +554,7 @@ export default function ProfilePage() {
                     </button> <br/>
 
                     <SaveChangesButton /> <br/>
+                    <DeleteConformationBox />
                     <DeleteAccountButton />
                 </div>
             </div>
